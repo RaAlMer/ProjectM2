99a790066fda15fe54bccdca12f256fbdd14705b
Belchior <belchior.fontao@gmail.com>
2022-02-17 20:59:47 +0000

    added the edit Post and the votes

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -36,10 +36,66 @@ router.get('/all', async (req, res) => {
   res.render('post/viewAllPost', { posts });
 });
 
+// Get route to edit the post
+
+router.get('/edit/:id', isLoggedIn, async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render('post/editPost', { post });
+});
+
+// Put route to edit the post
+
+router.put('/edit/:id', isLoggedIn, async (req, res) => {
+  req.post = await Post.findById(req.params.id);
+  req.post.title = req.body.title;
+  req.post.image = req.body.image;
+  req.post.description = req.body.description;
+  req.post.city = req.body.city;
+  req.post.country = req.body.country;
+  req.post.level = req.body.level;
+  req.post.longitude = req.body.longitude;
+  req.post.latitude = req.body.latitude;
+  req.post.user = req.session.currentUser._id;
+  try {
+    await req.post.save();
+    res.redirect('/posts/all');
+  } catch (error) {
+    res.redirect('/posts/edit/:id');
+  }
+});
+
+//Route for upVote
+
+router.get('/upvote/:id', isLoggedIn, async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  if (post.downVote.includes(req.session.currentUser._id)) {
+    post.downVote.splice(req.session.currentUser._id, 1);
+  }
+  if (!post.upVote.includes(req.session.currentUser._id)) {
+    post.upVote.push(req.session.currentUser._id);
+  }
+  post.save()
+  res.redirect("/posts/all")
+});
+
+//Route for downVote
+
+router.get('/downvote/:id', isLoggedIn, async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  if (post.upVote.includes(req.session.currentUser._id)) {
+    post.upVote.splice(req.session.currentUser._id, 1);
+  }
+  if (!post.downVote.includes(req.session.currentUser._id)) {
+    post.downVote.push(req.session.currentUser._id);
+  }
+  post.save()
+  res.redirect("/posts/all")
+});
+
 // DELETE route delete post
-router.delete('/:id', async (req, res) => {
-    await Post.findByIdAndDelete(req.params.id);
-    res.redirect('/posts/all')
+router.delete('/:id', isLoggedIn, async (req, res) => {
+  await Post.findByIdAndDelete(req.params.id);
+  res.redirect('/posts/all');
 });
 
 module.exports = router;
