4b485256b19389aca4712a71fbd8000001a46f3c
Raúl <raul.alonso.merino@gmail.com>
2022-02-26 10:10:08 +0100

    added map

--- models/map.model.js
+++
@@ -1,6 +0,0 @@
-const { Schema, model, SchemaTypes } = require('mongoose');
-const mapSchema = new Schema({
-  xy: [Number],
-});
-
-module.exports = model('Map', mapSchema);
--- routes/map.routes.js
+++ routes/map.routes.js
@@ -1,42 +1,12 @@
 const { Router } = require('express');
 const router = new Router();
-const Map = require('../models/map.model');
 const Post = require('../models/post.model');
 
-
-router.get("/", async (req,res)=>{
-    const posts = await Post.find()
-    const coordinates = []
-    posts.forEach(async (post)=>{
-       const coo = await Map.create({
-            xy: [post.latitude, post.longitude],
-          })
-          console.log(coo)
-          coordinates.push(coo)
-    })
-    console.log(coordinates)
-        res.render("map",{coordinates})
-
-})
-
-
-
-
-
-
-
-/* router.get('/', async (req, res) => {
-    const coordinates = await Map.find()
-    res.render('map', { coordinates })
-  })
-  
-  router.post('/', async (req, res) => {
-      console.log(Object.keys(req.body))
-    const coordinates = JSON.parse(Object.keys(req.body)[0])
-    console.log(coordinates)
-    await Map.create({
-      xy: [coordinates.lat, coordinates.lng],
-    })
-  }) */
-
-  module.exports = router;
+router.get('/', async (req, res) => {
+  const posts = await Post.find();
+  const coordinates = posts.map((post) => {
+    return [post.latitude, post.longitude];
+  });
+  res.render('map', { coordinates });
+});
+module.exports = router;
