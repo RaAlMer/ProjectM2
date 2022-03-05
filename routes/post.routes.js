fae60d6734e994039b92617ff19c599226e07b2e
Belchior <belchior.fontao@gmail.com>
2022-03-05 09:40:45 +0000

    fixed a problem in view all posts CSS

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -4,7 +4,6 @@ const Post = require('../models/post.model');
 const { isLoggedIn } = require('../middlewares/guard');
 const imgUploader = require('../cloudinary.config');
 const User = require('../models/user.model');
-const Comment = require('../models/comment.model');
 
 // GET route to create post
 router.get('/create', isLoggedIn, (req, res) => {
@@ -16,6 +15,7 @@ router.get('/create', isLoggedIn, (req, res) => {
 router.post(
   '/create',
   isLoggedIn,
+  // choose a max of 3 images
   imgUploader.array('image', 3),
   async (req, res) => {
     let preImage = [];
@@ -35,6 +35,7 @@ router.post(
     try {
       const user = await User.findById(req.session.currentUser._id);
       user.posts.push(req.post.id);
+      // add to the score system - each Post is equal to 10 points
       user.score += 10;
       await user.save();
       await req.post.save();
@@ -61,13 +62,19 @@ router.get('/edit/:id', isLoggedIn, async (req, res) => {
 router.put(
   '/edit/:id',
   isLoggedIn,
-  imgUploader.single('image'),
+  //upload max 3 images
+  imgUploader.array('image', 3),
   async (req, res) => {
     req.post = await Post.findById(req.params.id);
     req.post.title = req.body.title;
     req.post.status = req.body.status;
-    if (req.file) {
-      req.post.image = req.file.path;
+    //check if there are no images
+    if (req.files.length !== 0) {
+      let preImage = [];
+      req.files.forEach((img) => {
+        preImage.push(img.path);
+      });
+      req.post.image = preImage;
     }
     req.post.description = req.body.description;
     req.post.city = req.body.city;
@@ -88,8 +95,12 @@ router.put(
 // GET route for upVote
 router.get('/upvote/:id', isLoggedIn, async (req, res) => {
   const post = await Post.findById(req.params.id);
+  const user = await User.findById(post.user);
+  //check if the current user has already voted
   if (!post.upVote.includes(req.session.currentUser._id)) {
     post.upVote.push(req.session.currentUser._id);
+    // the user score is up by 5 points
+    user.score += 5;
     if (post.downVote.includes(req.session.currentUser._id)) {
       post.downVote.splice(
         post.downVote.indexOf(req.session.currentUser._id),
@@ -97,8 +108,6 @@ router.get('/upvote/:id', isLoggedIn, async (req, res) => {
       );
     }
   }
-  const user = await User.findById(post.user);
-  user.score += 5;
   user.save();
   post.save();
   res.redirect(req.get('referer'));
@@ -107,14 +116,15 @@ router.get('/upvote/:id', isLoggedIn, async (req, res) => {
 // GET route for downVote
 router.get('/downvote/:id', isLoggedIn, async (req, res) => {
   const post = await Post.findById(req.params.id);
+  const user = await User.findById(post.user);
   if (!post.downVote.includes(req.session.currentUser._id)) {
     post.downVote.push(req.session.currentUser._id);
+    // the user score is down by 5 points
+    user.score -= 5;
     if (post.upVote.includes(req.session.currentUser._id)) {
       post.upVote.splice(post.upVote.indexOf(req.session.currentUser._id), 1);
     }
   }
-  const user = await User.findById(post.user);
-  user.score -= 5;
   user.save();
   post.save();
   res.redirect(req.get('referer'));
@@ -124,6 +134,7 @@ router.get('/downvote/:id', isLoggedIn, async (req, res) => {
 router.delete('/:id', isLoggedIn, async (req, res) => {
   await Post.findByIdAndDelete(req.params.id);
   const user = await User.findById(req.session.currentUser._id);
+  // the user score is down by 10 points 
   user.score -= 10;
   await user.save();
   res.redirect('/posts/all');
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -11,23 +11,28 @@ const imgUploader = require('../cloudinary.config');
 
 // GET Sign-up/Log-in route
 router.get('/sign-log', (req, res) => {
+  //checks if someone is logged in
   if (req.session.currentUser) {
     res.redirect('/');
   } else {
-    res.render('user/logInSignUp', { message: '' });
+    res.render('user/logInSignUp', { message: '', errorMessage: '' });
   }
 });
 
 // POST route to create account
 router.post('/signup', async (req, res, next) => {
+  //checks if someone is logged in
   if (req.session.currentUser) {
     res.redirect('/');
   } else {
     const { username, email, password, repeatPassword } = req.body;
+    //check if both password are equal
     if (password === repeatPassword) {
       try {
+        //creating a hash password
         const salt = await bcrypt.genSalt(saltRounds);
         const hashedPassword = await bcrypt.hash(password, salt);
+        //creates an user
         const user = await User.create({
           username,
           email,
@@ -36,7 +41,11 @@ router.post('/signup', async (req, res, next) => {
         await user.save();
         res.redirect('/');
       } catch (error) {
-        res.redirect('/signup');
+        if (error.code === 11000) {
+          res.render('user/logInSignUp', {
+            errorMessage: 'The email already exists',
+          });
+        }
       }
     } else {
       res.redirect('/signup');
@@ -46,15 +55,18 @@ router.post('/signup', async (req, res, next) => {
 
 // POST route to log into your account
 router.post('/login', async (req, res) => {
+  //checks if someone is logged in
   if (req.session.currentUser) {
     res.redirect('/');
   } else {
     try {
+      //search for the user credentials
       const user = await User.findOne({ email: req.body.email });
       const isPwCorrect = await bcrypt.compare(
         req.body.password,
         user.password
       );
+      // if they are correct you log in
       if (isPwCorrect) {
         req.session.currentUser = user;
         res.redirect(`/profile/${user.id}`);
@@ -69,6 +81,7 @@ router.post('/login', async (req, res) => {
 
 // GET route to Profile page
 router.get('/profile/:id', isLoggedIn, async (req, res) => {
+  //go to the User profile
   const user = await User.findById(req.params.id);
   res.render('user/myprofile', { user });
 });
@@ -76,11 +89,12 @@ router.get('/profile/:id', isLoggedIn, async (req, res) => {
 // GET route to edit your profile
 router.get('/editProfile/:id', isLoggedIn, async (req, res) => {
   const user = await User.findById(req.params.id);
-  if (req.session.currentUser._id === user.id.toString()){
+  //check if the user in the session is the same as the User profile
+  if (req.session.currentUser._id === user.id.toString()) {
     res.render('user/editProfile', { user, errorMessage: '' });
   } else {
     res.redirect(`/profile/${req.params.id}`);
-  }  
+  }
 });
 
 // PUT route to edit your profile
@@ -91,6 +105,7 @@ router.put(
   async (req, res) => {
     req.user = await User.findById(req.params.id);
     req.user.username = req.body.username;
+    //checks if an image has been uploaded
     if (!req.file) {
       req.user.img = req.user.img;
     } else {
@@ -100,7 +115,6 @@ router.put(
     req.user.gender = req.body.gender;
     try {
       await req.user.save();
-
       res.redirect(`/profile/${req.user.id}`);
     } catch (error) {
       if (error.code === 11000) {
@@ -115,6 +129,7 @@ router.put(
 
 //GET route for logout
 router.get('/logout', (req, res) => {
+  //destroy the session
   req.session.destroy();
   res.redirect('/');
 });
